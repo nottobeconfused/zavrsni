5f31d602c389cb6f28d5cece807dd713113967d3
nottobeconfused <113766322+nottobeconfused@users.noreply.github.com>
2023-03-11 19:29:47 +0100

    zadnje dodano

--- client/src/scenes/navigacija/index.jsx
+++ client/src/scenes/navigacija/index.jsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Routes, Route, Link } from 'react-router-dom';
 import '../../App.css';
 import Naslovna from "../naslovna/Naslovna";
 import { OsobniProstor } from "../osobni-prostor/OsobniProstor";
+import axios from 'axios';
 
-const Navigacija = ({data}) => {
+const Navigacija = () => {
     const [isGrupeOpen, setIsGrupeOpen] = useState(false);
 
     const toggleGrupe = () => {
@@ -19,9 +20,46 @@ const Navigacija = ({data}) => {
   const handleItemClick = (item) => {
     setActiveItem(item);
   };
+  const [groups, setGroups] = useState([]);
 
+  useEffect(() => {
+    // Get user's groups from the backend API
+    axios.get('/api/groups')
+      .then(res => {
+        setGroups(res.data.groups);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
+  const [groupName, setGroupName] = useState('');
+  const [users, setUsers] = useState([]);
+  const [showModal, setShowModal] = useState(false);
 
-      const listItems = data;
+  const handleInputChange = e => {
+    setGroupName(e.target.value);
+  };
+
+  const handleAddUser = e => {
+    e.preventDefault();
+    const newUser = e.target.user.value;
+    setUsers([...users, newUser]);
+    e.target.user.value = '';
+  };
+
+  const handleCreateGroup = e => {
+    e.preventDefault();
+    axios.post('/api/groups', { name: groupName, users: users })
+      .then(res => {
+        console.log(res.data);
+        setShowModal(false);
+        setGroupName('');
+        setUsers([]);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
 
     return (
         <>
@@ -50,14 +88,40 @@ const Navigacija = ({data}) => {
     </header>
     {isGrupeOpen && (
     <div className="grupice">
-        {listItems.map(item => (
-            <Link className="link" to="/grupe1/:id">
-                <div key={item.id} className="gumb-nav gumb-grupe" onClick={handleClick}>
-                <i className="uil uil-polygon grupica"></i>
-                <p>{item.label}</p>
-                </div>
-            </Link>
-          ))}
+        {groups.length > 0 ? (
+            groups.map(group => (
+                <Link className="link" to="/grupe1/:id">
+                    <div key={group._id} className="gumb-nav gumb-grupe" onClick={handleClick}>
+                    <i className="uil uil-polygon grupica"></i>
+                    <p>{group.name}</p>
+                    </div>
+                </Link>
+              ))
+        ):(
+            <button onClick={() => setShowModal(true)}>Create New Group</button>
+        )}
+        {showModal && (
+        <div>
+          <form onSubmit={handleCreateGroup}>
+            <label>
+              Group Name:
+              <input type="text" value={groupName} onChange={handleInputChange} />
+            </label>
+            <label>
+              Add Users:
+              <ul>
+                {users.map(user => (
+                  <li key={user}>{user}</li>
+                ))}
+              </ul>
+              <input type="text" name="user" />
+              <button onClick={handleAddUser}>Add User</button>
+            </label>
+            <button type="submit">Create Group</button>
+          </form>
+          <button onClick={() => setShowModal(false)}>Cancel</button>
+        </div>
+      )}
     </div>
     )};
     <Routes>
